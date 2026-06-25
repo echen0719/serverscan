f1468e0284a03cb4a308bec6a09c87f9c2e5094b
Eric Chen <echen0719@gmail.com>
2026-06-25 18:38:10 -0400

    Version 1.0.2
    Updated so it creates the directory and config file at startup.

--- src/client/java/echen0719/serverscan/ServerscanClient.java
+++ src/client/java/echen0719/serverscan/ServerscanClient.java
@@ -1,9 +1,14 @@
 package echen0719.serverscan;
 
+import java.io.File;
+
+import echen0719.serverscan.utils.nativeUtil;
 import echen0719.serverscan.screens.pastScansScreen;
 import echen0719.serverscan.screens.scanScreen;
 import echen0719.serverscan.screens.viewServerScreen;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ServerscanClient implements ClientModInitializer {
 	public static scanScreen mainScreen;
@@ -13,5 +18,19 @@ public class ServerscanClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		File gameDir = FabricLoader.getInstance().getGameDirectory();
+        File folder = new File(gameDir, "serverscan");
+
+		new File(folder, "outputs").mkdirs();
+        new File(folder, "logs").mkdirs();
+        new File(folder, "config").mkdirs();
+
+		try {
+			nativeUtil.writeConfig(folder, null);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
