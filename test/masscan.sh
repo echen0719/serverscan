042147a24a69e31372eb74a8b11baa7cc98f43bd
Eric Chen <echen0719@gmail.com>
2026-06-27 20:09:16 -0400

    Moved to new repo
    This doesn't need to be here so yeah. It's going.

--- test/masscan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$EUID" -ne 0 ]; then
-  echo "Forgot to run as sudo boi"
-  exit 1
-fi
-
-masscan 0.0.0.0-255.255.255.255 -p 25565 --rate $1 --exclude 255.255.255.255 -oJ output.json
-rm paused.conf
